f1e55a010dcbc2a66b3f130458d7884d1f340fb6
Dima <alwayswannafly070400@mail.ru>
2019-11-30 13:11:56 +0300

    Макет дизайна

---
+++ app/src/main/java/com/ticket/ui/login/LoginScreen.kt
@@ -0,0 +1,13 @@
+package com.ticket.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ticket.R
+
+class LoginScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+    }
+}
