5d4fa046c1f8e36f00fd7005e180d89f5b257260
Dima <alwayswannafly070400@mail.ru>
2020-01-12 21:40:32 +0300

    Добавлен SharedPreference(userId)

--- app/src/main/java/com/ticket/utils/SharedPrefsHelper.kt
+++ app/src/main/java/com/ticket/utils/SharedPrefsHelper.kt
@@ -1,6 +1,7 @@
 package com.ticket.utils
 
 import android.content.Context
+import com.ticket.utils.SharedPrefsHelper.Companion.PREF_KEY_ID
 import com.ticket.utils.SharedPrefsHelper.Companion.PREF_KEY_LAUNCH
 import com.ticket.utils.SharedPrefsHelper.Companion.PREF_KEY_NAME
 
@@ -28,10 +29,23 @@ fun setUserName(context: Context, name: String) {
         .apply()
 }
 
+fun getUserId(context: Context): String? {
+    return context.getSharedPreferences(SharedPrefsHelper.PREF_KEY_USER, Context.MODE_PRIVATE)
+        .getString(PREF_KEY_ID, "id")
+}
+
+fun setUserId(context: Context, userId: String) {
+    context.getSharedPreferences(SharedPrefsHelper.PREF_KEY_USER, Context.MODE_PRIVATE)
+        .edit()
+        .putString(PREF_KEY_ID, userId)
+        .apply()
+}
+
 class SharedPrefsHelper{
     companion object {
         const val PREF_KEY_USER = "user_data"
         const val PREF_KEY_LAUNCH = "isFirstLaunch"
         const val PREF_KEY_NAME = "user_name"
+        const val PREF_KEY_ID = "id_name"
     }
 }
